e034316f383db5c8e0592e85d7e1ff48d5a4a10c
Boom <boom@flyingpackets.net>
2024-12-12 01:44:32 -0500

    Esc mode indicator, RGB stealth/visible hotkeys

--- keyboards/keychron/q3_max/ansi_encoder/keymaps/boom/keymap.c
+++ keyboards/keychron/q3_max/ansi_encoder/keymaps/boom/keymap.c
@@ -25,7 +25,9 @@ enum layers {
 };
 
 enum custom_keycodes {
-    WINLOCK = SAFE_RANGE
+    WINLOCK = SAFE_RANGE,
+    RGB_STEALTH,
+    RGB_VISIBLE,
 };
 
 // clang-format off
@@ -39,12 +41,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(MAC_FN),  RWIN_T(KC_APP),  KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [MAC_FN] = LAYOUT_tkl_ansi(
-        _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,    _______,    _______,  _______,  _______,
-        _______,  BT_HST1,  BT_HST2,  BT_HST3,  P2P4G,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,    _______,  _______,  _______,
-        RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,    _______,  _______,  _______,
-        _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,              _______,
-        _______,            _______,  _______,  _______,  _______,  BAT_LVL,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,              _______,
-        _______,  _______,  _______,                                _______,                                _______,  _______,  _______,    _______,    _______,  _______,  _______),
+        _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,      KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,    _______,    _______,  _______,  _______,
+        _______,  BT_HST1,  BT_HST2,  BT_HST3,  P2P4G,    _______,  _______,      _______,  _______,  _______,  _______,  _______,  _______,    _______,    _______,  _______,  _______,
+        RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  RGB_STEALTH,  _______,  _______,  _______,  _______,  _______,  _______,    _______,    _______,  _______,  _______,
+        _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  RGB_VISIBLE,  _______,  _______,  _______,  _______,  _______,              _______,
+        _______,            _______,  _______,  _______,  _______,  BAT_LVL,      NK_TOGG,  _______,  _______,  _______,  _______,              _______,              _______,
+        _______,  _______,  _______,                                _______,                                    _______,  _______,  _______,    _______,    _______,  _______,  _______),
 
     [WIN_BASE] = LAYOUT_tkl_ansi(
         KC_ESC,   KC_F1,       KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,      KC_F12,          KC_MUTE,    KC_PSCR,  WINLOCK,  KC_F13,
@@ -55,12 +57,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,  MO(WIN_FN),  KC_LALT,                                KC_SPC,                                 KC_RALT,  TG(WIN_FN),  RWIN_T(KC_APP),  KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [WIN_FN] = LAYOUT_tkl_ansi(
-        _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,    _______,    _______,  _______,  _______,
-        _______,  BT_HST1,  BT_HST2,  BT_HST3,  P2P4G,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,    KC_WH_L,  KC_WH_R,  KC_WH_U,
-        RGB_TOG,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,    KC_BTN1,  KC_BTN2,  KC_WH_D,
-        _______,  KC_BTN3,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,              _______,
-        _______,            KC_BTN2,  KC_BTN1,  _______,  _______,  BAT_LVL,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,              KC_MS_U,
-        _______,  _______,  _______,                                _______,                                _______,  _______,  _______,    _______,    KC_MS_L,  KC_MS_D,  KC_MS_R),
+        _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,      KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,    _______,    _______,  _______,  _______,
+        _______,  BT_HST1,  BT_HST2,  BT_HST3,  P2P4G,    _______,  _______,      _______,  _______,  _______,  _______,  _______,  _______,    _______,    KC_WH_L,  KC_WH_R,  KC_WH_U,
+        RGB_TOG,  _______,  _______,  _______,  _______,  _______,  RGB_STEALTH,  _______,  _______,  _______,  _______,  _______,  _______,    _______,    KC_BTN1,  KC_BTN2,  KC_WH_D,
+        _______,  KC_BTN3,  _______,  _______,  _______,  _______,  RGB_VISIBLE,  _______,  _______,  _______,  _______,  _______,              _______,
+        _______,            KC_BTN2,  KC_BTN1,  _______,  _______,  BAT_LVL,      NK_TOGG,  _______,  _______,  _______,  _______,              _______,              KC_MS_U,
+        _______,  _______,  _______,                                _______,                                    _______,  _______,  _______,    _______,    KC_MS_L,  KC_MS_D,  KC_MS_R),
 };
 
 // clang-format on
@@ -87,6 +89,22 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 }
             }
             return false;
+
+        // RGB stealth mode - set HSV to be all zero for retaining layer indicators
+        case RGB_STEALTH:
+            if (record->event.pressed) {
+                rgb_matrix_mode(RGB_MATRIX_SOLID_COLOR);
+                rgb_matrix_sethsv(0, 0, 0);
+            }
+            return false;
+
+        // RGB visible mode - make everything colorful and dazzling
+        case RGB_VISIBLE:
+            if (record->event.pressed) {
+                rgb_matrix_mode(RGB_MATRIX_CYCLE_UP_DOWN);
+                rgb_matrix_sethsv(0, 255, 255);
+            }
+            return false;
         
         // Let Keychron take the wheel
         default:
@@ -98,14 +116,20 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return true;
 }
 
-// Make arrow keys glow when mouse keys layer is active
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     const uint16_t arrow_key_lights[4] = {75, 84, 85, 86};
+    uint8_t current_layer = get_highest_layer(layer_state | default_layer_state);
+
+    // Make arrow keys glow when mouse keys layer is active
     for (uint8_t i = 0; i < 4; i++) {
-        if (get_highest_layer(layer_state | default_layer_state) == WIN_FN) {
+        if (current_layer == WIN_FN) {
             rgb_matrix_set_color(arrow_key_lights[i], RGB_WHITE);
-            // SET_LED_ON(arrow_key_lights[i]);
         }
+    }
+
+    // Escape key glow when on layer with mouse key bindings
+    if (current_layer == WIN_BASE || current_layer == WIN_FN) {
+        rgb_matrix_set_color(0, RGB_BLUE);
     }
     return false;
 }
