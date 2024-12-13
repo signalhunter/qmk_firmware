a1b7c5a371d7b799a5ccf5c504553df372cdfc6c
Boom <boom@flyingpackets.net>
2024-12-12 20:05:04 -0500

    Modify LWin mousekeys layer behavior

--- keyboards/keychron/q3_max/ansi_encoder/keymaps/boom/keymap.c
+++ keyboards/keychron/q3_max/ansi_encoder/keymaps/boom/keymap.c
@@ -63,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,        KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,     KC_RBRC,        KC_BSLS,    KC_DEL,   KC_END,   KC_PGDN,
         KC_CAPS,  KC_A,        KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,                     KC_ENT,
         KC_LSFT,               KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,                     KC_RSFT,              KC_UP,
-        KC_LCTL,  MO(WIN_FN),  KC_LALT,                                KC_SPC,                                 KC_RALT,  TG(WIN_FN),  TD(TD_WINAPP),  KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT),
+        KC_LCTL,  LT(3, KC_LWIN),  KC_LALT,                                KC_SPC,                                 KC_RALT,  TG(WIN_FN),  TD(TD_WINAPP),  KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [WIN_FN] = LAYOUT_tkl_ansi(
         _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,      KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,    _______,    _______,  _______,  _______,
@@ -89,7 +89,7 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [MAC_FN]     = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI)},
     [WIN_BASE]   = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
     [WIN_FN]     = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI)},
-    [MKEYS] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+    [MKEYS]      = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
 };
 #endif // ENCODER_MAP_ENABLE
 
