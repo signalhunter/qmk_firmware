974b410bd1547bd4054de88d01d0b2a3201fd163
Boom <boom@flyingpackets.net>
2025-01-06 00:15:02 +0000

    Flex F4/F14

--- keyboards/keychron/q3_max/ansi_encoder/keymaps/boom/keymap.c
+++ keyboards/keychron/q3_max/ansi_encoder/keymaps/boom/keymap.c
@@ -33,21 +33,23 @@ enum custom_keycodes {
 
 enum td_keycodes {
     TD_WINAPP,
+    TD_FLEXF4,
 };
 
 tap_dance_action_t tap_dance_actions[] = {
     [TD_WINAPP] = ACTION_TAP_DANCE_DOUBLE(KC_RWIN, KC_APP),
+    [TD_FLEXF4] = ACTION_TAP_DANCE_DOUBLE(KC_F4, KC_F14),
 };
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_tkl_ansi(
-        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,      KC_F12,         KC_MUTE,    KC_PSCR,  WINLOCK,  KC_F13,
-        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,     KC_EQL,         KC_BSPC,    KC_INS,   KC_HOME,  KC_PGUP,
-        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,     KC_RBRC,        KC_BSLS,    KC_DEL,   KC_END,   KC_PGDN,
-        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,                     KC_ENT,
-        KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,                     KC_RSFT,              KC_UP,
-        KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(MAC_FN),  TD(TD_WINAPP),  KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT),
+        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    TD(TD_FLEXF4),  KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,      KC_F12,         KC_MUTE,    KC_PSCR,  WINLOCK,  KC_F13,
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,           KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,     KC_EQL,         KC_BSPC,    KC_INS,   KC_HOME,  KC_PGUP,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,           KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,     KC_RBRC,        KC_BSLS,    KC_DEL,   KC_END,   KC_PGDN,
+        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,           KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,                     KC_ENT,
+        KC_LSFT,            KC_Z,     KC_X,     KC_C,           KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,                     KC_RSFT,              KC_UP,
+        KC_LCTL,  KC_LWIN,  KC_LALT,                                      KC_SPC,                                 KC_RALT,  MO(MAC_FN),  TD(TD_WINAPP),  KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [MAC_FN] = LAYOUT_tkl_ansi(
         _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,      KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,    _______,    _______,  TG(MKEYS),  _______,
@@ -58,12 +60,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  _______,  _______,                                _______,                                    _______,  _______,  _______,    _______,    _______,  _______,    _______),
 
     [WIN_BASE] = LAYOUT_tkl_ansi(
-        KC_ESC,   KC_F1,       KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,      KC_F12,         KC_MUTE,    KC_PSCR,  WINLOCK,  KC_F13,
-        KC_GRV,   KC_1,        KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,     KC_EQL,         KC_BSPC,    KC_INS,   KC_HOME,  KC_PGUP,
-        KC_TAB,   KC_Q,        KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,     KC_RBRC,        KC_BSLS,    KC_DEL,   KC_END,   KC_PGDN,
-        KC_CAPS,  KC_A,        KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,                     KC_ENT,
-        KC_LSFT,               KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,                     KC_RSFT,              KC_UP,
-        KC_LCTL,  LT(3, KC_LWIN),  KC_LALT,                                KC_SPC,                                 KC_RALT,  TG(WIN_FN),  TD(TD_WINAPP),  KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT),
+        KC_ESC,   KC_F1,       KC_F2,    KC_F3,    TD(TD_FLEXF4),  KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,      KC_F12,         KC_MUTE,    KC_PSCR,  WINLOCK,  KC_F13,
+        KC_GRV,   KC_1,        KC_2,     KC_3,     KC_4,           KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,     KC_EQL,         KC_BSPC,    KC_INS,   KC_HOME,  KC_PGUP,
+        KC_TAB,   KC_Q,        KC_W,     KC_E,     KC_R,           KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,     KC_RBRC,        KC_BSLS,    KC_DEL,   KC_END,   KC_PGDN,
+        KC_CAPS,  KC_A,        KC_S,     KC_D,     KC_F,           KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,                     KC_ENT,
+        KC_LSFT,               KC_Z,     KC_X,     KC_C,           KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,                     KC_RSFT,              KC_UP,
+        KC_LCTL,  LT(3, KC_LWIN),  KC_LALT,                                      KC_SPC,                                 KC_RALT,  TG(WIN_FN),  TD(TD_WINAPP),  KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [WIN_FN] = LAYOUT_tkl_ansi(
         _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,      KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,    _______,    _______,  _______,  _______,
